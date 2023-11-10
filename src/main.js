a7c7296aa18f8d3b4f5ba935fd570021d0d2b300
matthieuGravy <matthieugravy@gmail.com>
2023-11-10 10:15:15 +0100

    oups

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-import "./assets/js/plant/plant";
-import "./assets/js/game";
-import "./assets/js/topBar";
-import "./assets/js/panel";
--- src/vite.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  base: '/plant-clicker/',
-  root: 'src',
-  build: {
-    outDir: '../dist'
-  }
-}
